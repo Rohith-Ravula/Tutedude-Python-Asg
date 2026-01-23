542e815efafbc727d8e011aca26a1dd141ae7828
Rohith-Ravula <ravularohit45@gmail.com>
2026-01-23 11:35:59 +0530

    Assignment 1 Commit

---
+++ Assignment-1/Basic_Mathematical_Operations.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+Add= num1+num2
+Sub= num1-num2
+Mul= num1*num2
+Div= num1/num2
+print('Addition: ',Add)
+print('Subtraction: ',Sub)
+print('Multiplication: ',Mul)
+print('Division: ',round(Div,2))
---
+++ Assignment-1/Personalized_Greeting_program.py
@@ -0,0 +1,3 @@
+a=input('Enter first name: ')
+b=input('Enter second name: ')
+print('Hello, ',a,' ',b,'!',' Welcome to the Python program.',sep='')
